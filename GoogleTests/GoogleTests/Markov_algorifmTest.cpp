c3e048b8b7cceb9c43e0ad302a8773d37b4fa61e
hohlov06 <43324895+hohlov06@users.noreply.github.com>
2019-06-08 02:40:48 +0400

    update

--- GoogleTests/GoogleTests/Markov_algorifmTest.cpp
+++ GoogleTests/GoogleTests/Markov_algorifmTest.cpp
@@ -3,5 +3,114 @@
 // Для запуска этого файла раскомментировать , если закомментировано
 // Закомментировать во всех остальных cpp всё, что ниже #include "pch.h"
 // либо исключить из проекта все остальные cpp
-
+//
 //#include "../../Markov_algorifm/Markov_algorifm/MarkovString.h"
+//
+//#include <iostream>
+//#include <list>
+//
+//using namespace std;
+//
+//Markov<char> mark;
+//Markov<char> mark2;
+//Markov<int> mark3;
+//
+//TEST(resultTest, charT)
+//{
+//    EXPECT_EQ(mark.getData(), std::string("Я купил килограмм апельсинов в том ларьке"));
+//    EXPECT_EQ(mark2.getData(), std::string("|||||"));
+//}
+//
+//TEST(resultTest, intT)
+//{
+//    EXPECT_EQ(mark3.getData(), std::list<int>({1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}));
+//}
+//
+//
+//
+//template <typename T>
+//ostream& operator<<(ostream& stre, const std::list<T>& a) {
+//    for (auto item : a)
+//        stre << item << " ";
+//    stre << "\n";
+//    return stre;
+//}
+//
+//
+//int main(int argc, char** argv)
+//{
+//    std::list<int> a;
+//    auto b = a.begin();
+//    setlocale(LC_ALL, "Russian");
+//
+//
+//
+//
+//    mark.setData("Я купил кг Аов в Т М");
+//
+//    mark.addRule("А", "апельсин", false);
+//    mark.addRule("кг", "килограмм", false);
+//    mark.addRule("М", "магазинчике", false);
+//    mark.addRule("Т", "том", false);
+//    mark.addRule("магазинчике", "ларьке", true);
+//    mark.addRule("в том ларьке", "на том рынке", false);
+//
+//
+//    cout << mark.getData() << endl;
+//
+//    while (mark.isTerminated() != true)
+//    {
+//        mark.step();
+//        cout << mark.getData() << endl;
+//    }
+//
+//    //mark.run();
+//    //cout << mark.getData() << endl;
+//
+//    cout << "===================================================" << endl;
+//
+//
+//
+//    mark2.setData("101");
+//    mark2.addRule("1", "0|", false);
+//    mark2.addRule("|0", "0||", false);
+//    mark2.addRule("0", "", false);
+//
+//    cout << mark2.getData() << endl;
+//    while (mark2.isTerminated() != true)
+//    {
+//        mark2.step();
+//        cout << mark2.getData() << endl;
+//    }
+//
+//    //mark2.run();
+//        //cout << mark2.getData() << endl;
+//
+//
+//    cout << "===================================================" << endl;
+//
+//
+//    mark3.setData({ 5 });
+//    mark3.addRule({ 5 }, { 4, 4 }, false);
+//    mark3.addRule({ 4 }, { 3, 3 }, false);
+//    mark3.addRule({ 3 }, { 2, 2 }, false);
+//    mark3.addRule({ 2 }, { 1, 1 }, false);
+//
+//    cout << mark3.getData() << endl;
+//    while (mark3.isTerminated() != true)
+//    {
+//        mark3.step();
+//        cout << mark3.getData() << endl;
+//    }
+//
+//    //mark3.run();
+//    //cout << mark3.getData() << endl;
+//
+//    cout << "===================================================" << endl;
+//    cout << "ok" << endl;
+//    //system("Pause");
+//
+//
+//    ::testing::InitGoogleTest(&argc, argv);
+//    return RUN_ALL_TESTS();
+//}
--- GoogleTests/GoogleTests/PlistTest.cpp
+++ GoogleTests/GoogleTests/PlistTest.cpp
@@ -3,5 +3,182 @@
 // Для запуска этого файла раскомментировать , если закомментировано
 // Закомментировать во всех остальных cpp всё, что ниже #include "pch.h"
 // либо исключить из проекта все остальные cpp
-
+//
 //#include "../../Plist/Plist/Plist.h"
+//
+//using namespace std;
+//
+//
+//WrappedDouble d1(2.), d2(4.), d3(15.);
+//WrappedInt i1(4), i2(6), i3(9), i4(9);
+//WrappedComplex c1(Complex(2., 0.)), c2(Complex(2., 4.)), c3(Complex(0., 4.)), c4(Complex(7., 6.));
+//
+//auto dptr1 = make_shared<WrappedDouble>(d1);
+//auto dptr2 = make_shared<WrappedDouble>(d2);
+//auto dptr3 = make_shared<WrappedDouble>(d3);
+//
+//auto iptr1 = make_shared<WrappedInt>(i1);
+//auto iptr2 = make_shared<WrappedInt>(i2);
+//auto iptr3 = make_shared<WrappedInt>(i3);
+//auto iptr4 = make_shared<WrappedInt>(i4);
+//
+//auto cptr1 = make_shared<WrappedComplex>(c1);
+//auto cptr2 = make_shared<WrappedComplex>(c2);
+//auto cptr3 = make_shared<WrappedComplex>(c3);
+//auto cptr4 = make_shared<WrappedComplex>(c4);
+//
+//Plist li;
+//
+//class FillPlist_1
+//{
+//public:
+//
+//    FillPlist_1()
+//    {
+//        li.push_back(dptr1); //2.
+//        li.push_back(iptr1); //4
+//        li.push_front(cptr2); //<2, 4>
+//    }
+//
+//    void step_1()
+//    {
+//        li.drop(dptr1); //2.
+//        li.drop(dptr3); //15.
+//    }
+//
+//
+//    ~FillPlist_1()
+//    {
+//        li.clean();
+//    }
+//};
+//
+//
+//class FillPlist_2
+//{
+//public:
+//
+//    FillPlist_2()
+//    {
+//        li.push_back(dptr3); //15.
+//        li.push_back(cptr1); //<2,0>
+//        li.push_back(dptr3); // 15.
+//        li.push_front(cptr3);// <0, 4>
+//        li.push_back(dptr3); // 15.
+//    }
+//
+//    void step_1()
+//    {
+//        li.add(cptr2, li.end()->prev_ptr->prev_ptr); //<2,4>
+//    }
+//
+//    void step_2()
+//    {
+//        li.drop(dptr2); //4.
+//        li.drop(dptr3); //15.
+//    }
+//
+//    ~FillPlist_2()
+//    {
+//        li.clean();
+//    }
+//};
+//
+//
+//TEST(HasFunction, first)
+//{
+//    FillPlist_1 qwe;
+//
+//    EXPECT_EQ(li.has(cptr1), true);
+//    EXPECT_EQ(li.has(dptr3), false);
+//    EXPECT_EQ(li.has(dptr2), true);
+//
+//    qwe.step_1();
+//
+//    EXPECT_EQ(li.has(dptr1), false);
+//}
+//
+//TEST(HasFunction, second)
+//{
+//    FillPlist_2 qwe;
+//
+//    EXPECT_EQ(li.has(dptr1), true);
+//    EXPECT_EQ(li.has(dptr3), true);
+//
+//    qwe.step_1();
+//
+//    EXPECT_EQ(li.has(cptr2), true);
+//
+//    qwe.step_2();
+//
+//    EXPECT_EQ(li.has(dptr3), true);
+//    EXPECT_EQ(li.has(cptr4), false);
+//}
+//
+//
+//
+//int main(int argc, char** argv)
+//{
+//
+//
+//
+//
+//
+//    li.push_back(dptr1); //2.
+//    li.push_back(iptr1); //4
+//    li.push_front(cptr2); //<2, 4>
+//    li.print();
+//
+//    cout << "is ";
+//    cptr1->print(); // <2, 0> 
+//    cout << " here: " << li.has(cptr1) << endl; //true
+//
+//    cout << "is ";
+//    dptr3->print(); //15.
+//    cout << " here: " << li.has(dptr3) << endl; //false
+//
+//    cout << "is ";
+//    dptr2->print(); //4.
+//    cout << " here: " << li.has(dptr2) << endl; //true
+//
+//    //drop удаляет не все, а первый встреченный ptr
+//
+//    li.drop(dptr1); //2.
+//    li.drop(dptr3); //15.
+//    li.print();
+//    li.clean();
+//
+//
+//    li.push_back(dptr3); //15.
+//    li.push_back(cptr1); //<2,0>
+//    li.push_back(dptr3); // 15.
+//    li.push_front(cptr3);// <0, 4>
+//    li.push_back(dptr3); // 15.
+//    li.print();
+//    li.add(cptr2, li.end()->prev_ptr->prev_ptr); //<2,4>
+//    li.print();
+//
+//    cout << "is ";
+//    dptr1->print(); //2.
+//    cout << " here: " << li.has(dptr1) << endl;
+//
+//    cout << "is ";
+//    dptr3->print(); //15.
+//    cout << " here: " << li.has(dptr3) << endl;
+//
+//
+//    li.drop(dptr2); //4.
+//    li.drop(dptr3); //15.
+//    li.print();
+//
+//    cout << "ok" << endl;
+//
+//
+//    li.clean();
+//    cout << "ok" << endl;
+//
+//    li.print();
+//
+//    ::testing::InitGoogleTest(&argc, argv);
+//    return RUN_ALL_TESTS();
+//}
