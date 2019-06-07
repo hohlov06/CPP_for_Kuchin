0d4c9593c32ea6890b87c68dfcfd1d19e6893205
hohlov06 <43324895+hohlov06@users.noreply.github.com>
2019-06-07 18:47:04 +0400

    qwe

---
+++ GoogleTests/GoogleTests/BinaryTreeTest.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+
+// Для запуска этого файла раскомментировать , если закомментировано
+// Закомментировать во всех остальных cpp всё, что ниже #include "pch.h"
+// либо исключить из проекта все остальные cpp
+
+//#include "../../BinaryTree/BinaryTree/BinaryTree.h"
+
+//Tree<int, double> tree;
+//
+////data = key + 0.5;
+//tree.add(10, 10.5);
+//tree.add(5, 5.5);
+//tree.add(4, 4.5);
+//tree.add(12, 12.5);
+//tree.add(8, 8.5);
+//tree.add(7, 7.5);
+//tree.add(9, 9.5);
+//tree.add(15, 15.5);
