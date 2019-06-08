5a5b8d760d1240e07d419cfc0176717963b8f633
hohlov06 <43324895+hohlov06@users.noreply.github.com>
2019-06-08 22:17:17 +0400

    тесты

--- GoogleTests/GoogleTests/Sector_containerTest.cpp
+++ GoogleTests/GoogleTests/Sector_containerTest.cpp
@@ -3,5 +3,173 @@
 // Для запуска этого файла раскомментировать , если закомментировано
 // Закомментировать во всех остальных cpp всё, что ниже #include "pch.h"
 // либо исключить из проекта все остальные cpp
-
+//
 //#include "../../Sector_container/Sector_container/SectorContainer.h"
+//
+//
+//#include <iostream>
+//#include <utility>
+//#include <vector>
+//
+//using namespace std;
+//
+//template <bool flag>
+//class TestCase
+//{
+//public:
+//    SectorContainer<flag>& cont;
+//    TestCase(SectorContainer<flag>& cont_) : cont(cont_)
+//    {
+//        cont.clear();
+//        cont.add({ 0,5 });
+//        cont.add({ 12,12 });
+//        cont.add({ 6,9 });
+//        cont.add({ 20,30 });
+//        cont.add({40,100000});
+//        cont.print();
+//    }
+//
+//    void step_one()
+//    {
+//        cont.add({ -99,-90 });
+//        cont.add({ -80,-70 });
+//        cont.add({ -60,-50 });
+//        cont.add({ -40,-30 });
+//        cont.add({ -20,-10 });
+//        cont.add({ -100, -20 });
+//        cont.add({ -99, -21 });
+//        cont.print();
+//    }
+//
+//    void step_two()
+//    {
+//        cont.add({ -5,-1 });
+//        cont.add({ -1,0 });
+//        cont.add({ 5,6 });
+//        cont.add({ 5,5 });
+//        cont.add({12 , 12});
+//        cont.print();
+//    }
+//
+//    ~TestCase()
+//    {
+//        cont.clear();
+//    }
+//};
+//
+//
+//TEST(WithExceptionTests, Exceptions)
+//{
+//    SectorContainer<true> cont;
+//    TestCase<true> qwe(cont);
+//
+//    ASSERT_THROW(cont.add({0,2}), std::domain_error);
+//    ASSERT_THROW(cont.add({2,2}), std::domain_error);
+//    ASSERT_THROW(cont.add({12,12}), std::domain_error);
+//    ASSERT_THROW(cont.add({ -1,10 }), std::domain_error);
+//    ASSERT_THROW(cont.add({ 5,6 }), std::domain_error);
+//    ASSERT_THROW(cont.add({ 1,2 }), std::domain_error);
+//    ASSERT_THROW(cont.add({-1, 100000000}), std::domain_error);
+//}
+//
+//TEST(WithExceptionTests, findFunction)
+//{
+//    SectorContainer<true> cont;
+//    TestCase<true> qwe(cont);
+//
+//    EXPECT_EQ(sector(*cont.find(0) ), sector(0,5));
+//    EXPECT_EQ(sector(*cont.find(2) ), sector(0,5));
+//    EXPECT_EQ(sector(*cont.find(5) ), sector(0,5));
+//    EXPECT_EQ(sector(*cont.find(6) ), sector(6,9));
+//    EXPECT_EQ(sector(*cont.find(9) ), sector(6,9));
+//    EXPECT_EQ(sector(*cont.find(50)), sector(40, 100000));
+//    EXPECT_EQ(cont.find(-1), cont.end());
+//    EXPECT_EQ(cont.find(10), cont.end());
+//    EXPECT_EQ(cont.find(11), cont.end());
+//    EXPECT_EQ(cont.find(15), cont.end());
+//    EXPECT_EQ(cont.find(100001), cont.end());
+//    EXPECT_EQ(cont.find(35), cont.end());
+//}
+//
+//
+//TEST(NoExceptionTests, mergeTest)
+//{
+//    SectorContainer<false> cont;
+//    TestCase<false> qwe(cont);
+//    qwe.step_one();
+//    EXPECT_EQ(sector(*cont.begin()), sector(-100,-10));
+//    
+//    cont.erase(cont.begin());
+//    SectorContainer<false> cont2;
+//    TestCase<false> qwe2(cont2);
+//    EXPECT_EQ(cont, cont2);
+//}
+//
+//
+//TEST(NoExceptionTests, findFunction)
+//{
+//    SectorContainer<false> cont;
+//    TestCase<false> qwe(cont);
+//    qwe.step_one();
+//    qwe.step_two();
+//
+//    EXPECT_EQ(sector(*cont.find(-10)), sector(-100,-10));
+//    EXPECT_EQ(sector(*cont.find(-3)), sector(-5,9));
+//    EXPECT_EQ(cont.find(-7), cont.end() );
+//    EXPECT_EQ(cont.find(10), cont.end() );
+//    
+//    auto it = cont.begin();
+//    EXPECT_EQ(sector(*it), sector(-100, -10) ); 
+//    it++;
+//    EXPECT_EQ(sector(*it), sector(-5,9 ));
+//    it++;
+//    EXPECT_EQ(sector(*it), sector(12, 12));
+//    it++;
+//    EXPECT_EQ(sector(*it), sector(20, 30));
+//    it++;
+//    EXPECT_EQ(sector(*it), sector(40,100000 ));
+//    it++;
+//    EXPECT_EQ(it, cont.end());
+//}
+//
+//
+//
+//
+//int main(int argc, char** argv)
+//{
+//    //по умолчанию считаем пару уже упорядоченной, т.е. first <= second
+//    try
+//    {
+//        //SimpleVector vec;
+//
+//        //vec.add({ 1,2 });
+//        //vec.add({ 0,2 });
+//        //vec.add({ 6,7 });
+//        //auto val = vec.find(3);
+//
+//        SectorContainer<false> sect; // шаблонный параметр true - кидаются исключения при пересечении отрезков,
+//                                    //                    false(по умолчанию) - слияние пересекающихся в один
+//        sector a(1, 4);
+//        sector b(3, 7);
+//        sect.add(a);
+//        sect.add(b);
+//        sect.add(sector(8, 9));
+//        sect.add({ 13,15 });
+//        sect.add({ { 0,1 }, { 9,13 }, {16,19} });
+//        std::vector<sector> vecsec({ {-9,-8}, {-6,1} });
+//        sect.add(vecsec);
+//
+//        auto it = sect.find(16);
+//
+//        sect.print();
+//    }
+//    catch (std::exception& e)
+//    {
+//        cout << e.what() << endl;
+//        //std::abort();
+//    }
+//
+//
+//    ::testing::InitGoogleTest(&argc, argv);
+//    return RUN_ALL_TESTS();
+//}
--- GoogleTests/GoogleTests/Time_formatTest.cpp
+++ GoogleTests/GoogleTests/Time_formatTest.cpp
@@ -3,5 +3,118 @@
 // Для запуска этого файла раскомментировать , если закомментировано
 // Закомментировать во всех остальных cpp всё, что ниже #include "pch.h"
 // либо исключить из проекта все остальные cpp
-
+//
 //#include "../../Time_format/Time_format/Time.h"
+//#include <iostream>
+//
+//using namespace std;
+//using namespace DateTime;
+//
+//
+//TEST(TimeTest, CheckExceptionsDate)
+//{
+//    ASSERT_THROW(Time(0,0,32,1,1), std::domain_error);
+//    ASSERT_THROW(Time(61,1,1,1,1), std::domain_error);
+//    ASSERT_THROW(Time(1,-1,1,1,1), std::domain_error);
+//    ASSERT_THROW(Time(0,0,1,13,1), std::domain_error);
+//    ASSERT_THROW(Time(0,0,31,4,2), std::domain_error);
+//    ASSERT_THROW(Time(0,0,29,2,2019), std::domain_error);
+//    ASSERT_THROW(Time(0,0,30,2,2020), std::domain_error);
+//    ASSERT_THROW(Time(0,0,31,11,2010), std::domain_error);
+//    ASSERT_THROW(Time(0,0,31,9,2010), std::domain_error);
+//}
+//
+//TEST(TimeTest, CheckExceptionsSum)
+//{
+//    ASSERT_THROW(Time(0, 0, 62, 1, 1, true, true), std::domain_error);
+//    ASSERT_THROW(Time(0, 0, 1, 24, 1, true, true), std::domain_error);
+//    ASSERT_THROW(Time(0, 0, -30, 1, 1, true, true), std::domain_error);
+//}
+//
+//TEST(Constructors, TimeIntervalTest)
+//{
+//    EXPECT_EQ(TimeInterval(60, 0, 0, 0, 0), TimeInterval(0, 1, 0, 0, 0));
+//    EXPECT_EQ(TimeInterval(-1, 0, 0, 0, 0), TimeInterval(59, 23, 30, 11, -1));
+//    EXPECT_EQ(TimeInterval(0, 24, 0, 0, 0), TimeInterval(0, 0, 1, 0, 0));
+//    EXPECT_EQ(TimeInterval(0, 0, 31, 0, 0), TimeInterval(0, 0, 0, 1, 0));
+//    EXPECT_EQ(TimeInterval(0, 0, 0, 12, 0), TimeInterval(0, 0, 0, 0, 1));
+//    EXPECT_EQ(TimeInterval(185, 0, 0, 0, 0), TimeInterval(5, 3, 0, 0, 0));
+//    EXPECT_EQ(TimeInterval(-121, 0, 0, 0, 0), TimeInterval(59, 21, 30, 11, -1));
+//    EXPECT_EQ(TimeInterval(0, 0, 0, 0, 0), TimeInterval(0, 0, 0, 0, 0));
+//}
+//
+//TEST(Constructors, TimeTest)
+//{
+//    EXPECT_EQ(Time(0, 0, 0, 1, 1, false), Time(0, 0, 31, 12, 0, false));
+//    EXPECT_EQ(Time(0, 0, 32, 12, 1, false), Time(0, 0, 1, 1, 2, false));
+//    EXPECT_EQ(Time(0, 0, 29, 2, 2020, false), Time(0, 0, 29, 2, 2020, false));
+//    EXPECT_EQ(Time(0, 0, 29, 2, 2019, false), Time(0, 0, 1, 3, 2019, false));
+//    EXPECT_EQ(Time(0, 0, 0, 3, 2020, false), Time(0, 0, 29, 2, 2020, false));
+//    EXPECT_EQ(Time(0, 0, 0, 3, 2019, false), Time(0, 0, 28, 2, 2019, false));
+//    EXPECT_EQ(Time(0, 0, 32, 3, 1, false), Time(0, 0, 1, 4, 1, false));
+//    EXPECT_EQ(Time(0, 0, 31, 4, 1, false), Time(0, 0, 1, 5, 1, false));
+//    EXPECT_EQ(Time(0, 0, 31, 8, 1, false), Time(0, 0, 31, 8, 1, false));
+//    EXPECT_EQ(Time(0, 0, 0, 10, 1, false), Time(0, 0, 30, 9, 1, false));
+//}
+//
+//
+//
+//TEST(Operators, T_plus_TI)
+//{
+//    EXPECT_EQ(Time(30, 12, 15, 6, 0) + TimeInterval(1, 1, 1, 1, 1) , Time(31, 13, 16, 7, 1));
+//    EXPECT_EQ(Time(30, 12, 15, 6, 0) + TimeInterval(40, 13, 20, 10, 1) , Time(10, 2, 6, 5, 2));
+//}
+//
+//TEST(Operators, T_minus_TI)
+//{
+//    EXPECT_EQ(Time(30, 12, 15, 6, 0) - TimeInterval(1, 1, 1, 1, 1), Time(29, 11, 14, 5, -1));
+//    EXPECT_EQ(Time(30, 12, 15, 6, 2) - TimeInterval(40, 13, 20, 10, 1), Time(50, 22, 25, 7, 0));
+//}
+//
+//TEST(Operators, T_minus_T)
+//{
+//    EXPECT_EQ(Time(30, 12, 15, 6, 0) - Time(1, 1, 1, 1, 1), TimeInterval(29, 11, 14, 5, -1));
+//    EXPECT_EQ(Time(30, 12, 15, 6, 2) - Time(40, 13, 20, 10, 1), TimeInterval(50, 22, 25, 7, 0));
+//}
+//
+//TEST(Operators, TI_plus_TI)
+//{
+//    EXPECT_EQ(TimeInterval(30, 12, 15, 6, 0) + TimeInterval(1, 1, 1, 1, 1), TimeInterval(31, 13, 16, 7, 1));
+//    EXPECT_EQ(TimeInterval(30, 12, 15, 6, 0) + TimeInterval(40, 13, 20, 10, 1), TimeInterval(10, 2, 5, 5, 2));
+//}
+//
+//TEST(Operators, TI_minus_TI)
+//{
+//    EXPECT_EQ(TimeInterval(30, 12, 15, 6, 0) - TimeInterval(1, 1, 1, 1, 1), TimeInterval(29, 11, 14, 5, -1));
+//    EXPECT_EQ(TimeInterval(30, 12, 15, 6, 2) - TimeInterval(40, 13, 20, 10, 1), TimeInterval(50, 22, 25, 7, 0));
+//}
+//
+//
+//
+//int main(int argc, char** argv)
+//{
+//
+//    try {
+//        cout << TimeInterval(30, 12, 15, 6, 0) + TimeInterval(1, 1, 1, 1, 1) << endl;
+//        cout << TimeInterval(30, 12, 15, 6, 0) + TimeInterval(40, 13, 20, 10, 1) << endl;
+//        Time t1(0, 0, 32, 1, 2019, false);
+//        Time t2(0, 0, 1, 1, 2019, false);
+//        TimeInterval ti1(0, 0, 10, 1, 1);
+//        TimeInterval ti2(1, 0, 10, 1, 1);
+//        cout << t1 << endl;
+//        cout << t2 << endl;
+//        /*t1.setEuropeanFormat(false);
+//        cout << t1 << endl;*/
+//
+//        TimeInterval t = t1 - t2;
+//        //Time tt = t1 + t2;
+//        cout << t << endl;
+//        //TimeInterval ti2 = ti;
+//        //t = t + ti;
+//        //cout << (t + ti )<< endl;
+//    }
+//    catch (std::exception& e) { cout << e.what(); }
+//
+//    ::testing::InitGoogleTest(&argc, argv);
+//    return RUN_ALL_TESTS();
+//}
